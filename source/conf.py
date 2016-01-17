aa1dcd62a8da94cd0570327cc1a755a46597050d
谢文龙 <xiewenlongs@gmail.com>
2016-01-17 16:44:28 +0800

    update doc

--- source/conf.py
+++ source/conf.py
@@ -12,7 +12,7 @@
 # All configuration values have a default; values that are commented out
 # serve to show the default.
 
-import sphinx_bootstrap_theme
+# import sphinx_bootstrap_theme
 
 
 # If extensions (or modules to document with autodoc) are in another directory,
@@ -120,7 +120,7 @@ html_theme = 'sphinx_rtd_theme'
 # }
 
 # Add any paths that contain custom themes here, relative to this directory.
-html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
+# html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
